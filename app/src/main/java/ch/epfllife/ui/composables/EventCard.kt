1911d5d0d881f8965f459daa2c0a46e40931fe34
kianbahia <kianhbahia@gmail.com>
2025-10-13 18:04:35 +0200

    Apply ktfmt formatting fixes

--- app/src/main/java/ch/epfllife/ui/composables/EventCard.kt
+++ app/src/main/java/ch/epfllife/ui/composables/EventCard.kt
@@ -16,10 +16,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ch.epfllife.model.entities.Event
-import ch.epfllife.model.map.Location
 
 @Composable
 fun EventCard(event: Event, modifier: Modifier = Modifier, onClick: () -> Unit = {}) {
