1a4ba543dcc132eabe3113d5824ab12ac1828d10
Mateo-Rico <mateo.rico.cdm@gmail.com>
2025-10-17 12:09:11 +0200

    Add tests for HomeScreen with Tags

---
+++ app/src/androidTest/java/ch/epfllife/ui/home/HomeScreenTest.kt
@@ -0,0 +1,45 @@
+package ch.epfllife.ui.home
+
+import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.assertIsDisplayed
+// import androidx.compose.ui.test.assertExists
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import ch.epfllife.ui.composables.DisplayedEventsTestTags
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * This test class verifies the correct behavior and visual rendering of the HomeScreen composable.
+ * It ensures that elements such as the logo, search bar, filter buttons, and event list behave as expected.
+ */
+class HomeScreenTest {
+
+    @get:Rule val composeTestRule = createComposeRule()
+
+    @Before
+    fun setUp() {
+        // Prepare the composable before each test
+        composeTestRule.setContent { HomeScreen() }
+    }
+
+    @Test
+    fun testUiElementsDisplayed() {
+        arrayOf(HomeScreenTestTags.EPFLLOGO, DisplayedEventsTestTags.BUTTON_ALL,
+            DisplayedEventsTestTags.BUTTON_SUBSCRIBED).forEach {
+            assertDisplayed(it)
+        }
+    }
+
+    fun assertDisplayed(testTag: String) {
+        composeTestRule
+            .onNodeWithTag(testTag)
+            .assertIsDisplayed()
+    }
+
+}
