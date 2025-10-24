20478d1af6ebbd644bf679793204777f41a39a4d
Adriv020 <avicosan9@alumnes.ub.edu>
2025-10-25 00:06:03 +0200

    refactor(ui): wrap content in Box to handle padding

--- app/src/main/java/ch/epfllife/ui/myevents/MyEvents.kt
+++ app/src/main/java/ch/epfllife/ui/myevents/MyEvents.kt
@@ -1,9 +1,15 @@
 package ch.epfllife.ui.myevents
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
-fun MyEvents() {
-  Text(text = "MyEvents")
+fun MyEvents(modifier: Modifier = Modifier) {
+  Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    Text(text = "MyEvents")
+  }
 }
