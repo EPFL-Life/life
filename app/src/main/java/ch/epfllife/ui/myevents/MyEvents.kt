0f462165813ee78485fde68f25723d305f672ceb
Adriv020 <avicosan9@alumnes.ub.edu>
2025-10-24 23:20:25 +0200

    refactor(ui): remove per-screen Scaffolds

--- app/src/main/java/ch/epfllife/ui/myevents/MyEvents.kt
+++ app/src/main/java/ch/epfllife/ui/myevents/MyEvents.kt
@@ -1,27 +1,9 @@
 package ch.epfllife.ui.myevents
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
-import ch.epfllife.ui.navigation.BottomNavigationMenu
-import ch.epfllife.ui.navigation.NavigationActions
-import ch.epfllife.ui.navigation.NavigationTestTags
-import ch.epfllife.ui.navigation.Tab
 
 @Composable
-fun MyEvents(
-    navigationActions: NavigationActions? = null,
-) {
-  Scaffold(
-      bottomBar = {
-        BottomNavigationMenu(
-            selectedTab = Tab.Settings,
-            onTabSelected = { tab -> navigationActions?.navigateTo(tab.destination) },
-            modifier = Modifier.testTag(NavigationTestTags.BOTTOM_NAVIGATION_MENU))
-      }) { innerPadding ->
-        Text(text = "MyEvents", modifier = Modifier.padding(innerPadding))
-      }
+fun MyEvents() {
+  Text(text = "MyEvents")
 }
