17c0cc6a82da3f413d5fe0003815e667dabe36d6
daniel <git.daniel@protonmail.com>
2025-10-27 14:10:40 +0100

    feat: logic for association local repo - added all CRUD operations for repo - added getEventsForAssociation

--- app/src/main/java/ch/epfllife/model/association/AssociationRepositoryLocal.kt
+++ app/src/main/java/ch/epfllife/model/association/AssociationRepositoryLocal.kt
@@ -1,26 +1,58 @@
 package ch.epfllife.model.association
 
 import ch.epfllife.model.event.Event
+import ch.epfllife.model.event.EventRepository
 
-class AssociationRepositoryLocal : AssociationRepository {
+// TODO this works but is it a good approach?
+// we pass the eventRepository because the getEventsForAssociation function retrieves events from it
+class AssociationRepositoryLocal(private val eventRepository: EventRepository) :
+    AssociationRepository {
 
+  // In-memory storage for associations (use this only for testing)
+  private val associations = mutableListOf<Association>()
+  private var counter = 0
+
+  override fun getNewUid(): String {
+    return counter++.toString()
+  }
+
+  // TODO should associationId be String or Int?
   override suspend fun getAssociation(associationId: String): Association? {
-    TODO("Not yet implemented")
+    return associations.find { it.id == associationId }
   }
 
   override suspend fun getAllAssociations(): List<Association> {
-    TODO("Not yet implemented")
+    return associations.toList()
   }
 
   override suspend fun createAssociation(association: Association) {
-    TODO("Not yet implemented")
+    // TODO should some check (Nullcheck, duplciateId Check) be added?
+    associations.add(association)
   }
 
-  override suspend fun updateAssociation(associationId: String, newAssociation: Association) {
-    TODO("Not yet implemented")
+  override suspend fun updateAssociation(newAssociation: Association) {
+    val toBeUpdatedId = newAssociation.id
+
+    // check if id exists
+    if (!associations.any() { it.id == toBeUpdatedId }) {
+      throw NoSuchElementException(
+          "Association with id $toBeUpdatedId not found! Can NOT be updated!")
+    }
+
+    // replace the old association with the new one (based on ID)
+    associations.removeIf { it.id == toBeUpdatedId }
+    associations.add(newAssociation)
   }
 
+  // TODO: same here useString or Int for ID?
   override suspend fun getEventsForAssociation(associationId: String): List<Event> {
-    TODO("Not yet implemented")
+
+    if (!associations.any() { it.id == associationId }) {
+      throw NoSuchElementException(
+          "Association with id $associationId not found! Can NOT return list of events!")
+    }
+
+    // return list of events
+    return eventRepository.getAllEvents().filter() { it.associationId == associationId }
   }
 }
