c1659c1f0e1159dc31ef966606c6737b0e06703d
daniel <git.daniel@protonmail.com>
2025-10-27 14:09:22 +0100

    feat: prepared repository for logic implementation - added FirestoreConstants.kt with all Collections as constants to keep everything centralized - added getUid to allow for unique ID of associations

---
+++ app/src/main/java/ch/epfllife/model/firestore/FirebaseConstants.kt
@@ -0,0 +1,8 @@
+package ch.epfllife.model.firestore
+
+// This object holds all collection path constants for Firestore
+object FirestoreCollections {
+  const val EVENTS = "events"
+  const val ASSOCIATIONS = "associations"
+  const val USERS = "users"
+}
