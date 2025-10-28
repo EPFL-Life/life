7bd9ff070c14abc3feff0aefe621a8438a106d46
Alec Ghost <alexander.ghost@web.de>
2025-10-28 19:42:02 +0100

    WIP: Association Card display tests

--- app/src/androidTest/java/ch/epfllife/ui/composables/AssociationCardTest.kt
+++ app/src/androidTest/java/ch/epfllife/ui/composables/AssociationCardTest.kt
@@ -1,6 +1,9 @@
 package ch.epfllife.ui.composables
 
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import ch.epfllife.model.association.Association
 import ch.epfllife.model.event.EventCategory
 import ch.epfllife.utils.assertClickable
@@ -24,5 +27,25 @@ class AssociationCardTest {
         { clickHandler -> AssociationCard(association, onClick = clickHandler) },
         AssociationCardTestTags.ASSOCIATION_CARD,
     )
+  }
+
+  @Test
+  fun contentIsDisplayed() {
+    composeTestRule.setContent {
+      AssociationCard(association, onClick = {})
+    }
+    composeTestRule.waitUntil {
+      composeTestRule.onNodeWithTag(AssociationCardTestTags.ASSOCIATION_CARD).isDisplayed()
+    }
+
+    composeTestRule
+      .onNodeWithTag(AssociationCardTestTags.ASSOCIATION_NAME)
+      .assertExists()
+      .assertTextEquals(association.name)
+
+    composeTestRule
+      .onNodeWithTag(AssociationCardTestTags.ASSOCIATION_DESCRIPTION)
+      .assertExists()
+      .assertTextEquals(association.description)
   }
 }
