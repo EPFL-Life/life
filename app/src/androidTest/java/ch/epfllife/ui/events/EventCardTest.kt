86321db676121f6c0ed183e36af3141fe72c910f
gustavonsberg <github.thirsting835@simplelogin.fr>
2025-10-31 11:22:15 +0100

    Fixing some testes

--- app/src/androidTest/java/ch/epfllife/ui/events/EventCardTest.kt
+++ app/src/androidTest/java/ch/epfllife/ui/events/EventCardTest.kt
@@ -1,7 +1,9 @@
 package ch.epfllife.ui.events
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import ch.epfllife.model.association.Association
 import ch.epfllife.model.event.Event
+import ch.epfllife.model.event.EventCategory
 import ch.epfllife.model.map.Location
 import ch.epfllife.ui.composables.EventCard
 import ch.epfllife.ui.composables.EventCardTestTags
@@ -12,24 +14,29 @@ import org.junit.Test
 
 class EventCardTest {
 
-  @get:Rule val composeTestRule = createComposeRule()
-  private val event =
-      Event(
-          id = "0",
-          title = "Test Event",
-          description = "This is a test event",
-          location = Location(46.520278, 6.565556, "EPFL"),
-          time = Timestamp.now().toString(),
-          associationId = "assoc1",
-          tags = emptySet(),
-          price = 0u,
-      )
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    private val event =
+        Event(
+            id = "0",
+            title = "Test Event",
+            description = "This is a test event",
+            location = Location(46.520278, 6.565556, "EPFL"),
+            time = Timestamp.now().toString(),
+            association = Association(
+                name = "TestAssociation",
+                id = "gejn82",
+                description = "This is a test", eventCategory = EventCategory.ACADEMIC
+            ),
+            tags = emptySet(),
+            price = 0u,
+        )
 
-  @Test
-  fun isClickable() {
-    composeTestRule.assertClickable(
-        { clickHandler -> EventCard(event, onClick = clickHandler) },
-        EventCardTestTags.EVENT_CARD,
-    )
-  }
+    @Test
+    fun isClickable() {
+        composeTestRule.assertClickable(
+            { clickHandler -> EventCard(event, onClick = clickHandler) },
+            EventCardTestTags.EVENT_CARD,
+        )
+    }
 }
