db90778cd936709afa2fd5bbb14127a97c0d7880
gustavonsberg <github.thirsting835@simplelogin.fr>
2025-11-03 15:54:05 +0100

    pkmn

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -224,7 +224,7 @@ tasks.register("jacocoTestReport", JacocoReport::class) {
     description = "Generate Jacoco coverage reports"
     group = "test"
 
-    //mustRunAfter("testDebugUnitTest", "connectedDebugAndroidTest")
+    mustRunAfter("testDebugUnitTest", "connectedDebugAndroidTest")
 
     reports {
         xml.required = true
