ee498bfe7b44ed1b5b885bc3670d15c9199425b1
Mateo-Rico <mateo.rico.cdm@gmail.com>
2025-11-07 11:43:56 +0100

    fix: modified event init in tests to fir event model

--- app/src/androidTest/java/ch/epfllife/example_data/ExampleEvents.kt
+++ app/src/androidTest/java/ch/epfllife/example_data/ExampleEvents.kt
@@ -2,6 +2,7 @@ package ch.epfllife.example_data
 
 import ch.epfllife.model.event.Event
 import ch.epfllife.model.map.Location
+import ch.epfllife.ui.composables.Price
 
 /**
  * A singleton object holding sample event data for testing purposes. We localize these event to
@@ -21,9 +22,9 @@ object ExampleEvents {
           description = "Join us for an exciting symposium on satellite technology.",
           location = satelliteLocation,
           time = "2025-11-20 09:00",
-          association = ExampleAssociations.association1,
-          tags = listOf("tech", "symposium", "space"),
-          price = 25u, // Using UInt
+          association = ExampleAssociation.association1,
+          tags = setOf("tech", "symposium", "space"),
+          price = Price(25u), // Using UInt
           pictureUrl = "https://actu.epfl.ch/image/76257/original/5616x3744.jpg")
 
   val event2 =
@@ -33,9 +34,9 @@ object ExampleEvents {
           description = "Annual music festival at EPFL. Don't miss out!",
           location = epflCampusLocation,
           time = "2026-05-08 18:00",
-          association = ExampleAssociations.association2,
-          tags = listOf("music", "festival", "party"),
-          price = 45u,
+          association = ExampleAssociation.association2,
+          tags = setOf("music", "festival", "party"),
+          price = Price(45u),
           pictureUrl =
               "https://www.jambase.com/wp-content/uploads/2023/04/332066973_940895027072259_250410373047721675_n-e1680722695547-1480x832.jpg")
 
@@ -48,9 +49,9 @@ object ExampleEvents {
           location =
               Location(latitude = 46.5193, longitude = 6.5656, name = "Rolex Learning Center"),
           time = "2025-12-05 12:30",
-          association = ExampleAssociations.association3,
-          tags = listOf("food", "networking", "free"),
-          price = 0u, // Free event
+          association = ExampleAssociation.association3,
+          tags = setOf("food", "networking", "free"),
+          price = Price(0u), // Free event
           pictureUrl = null // No image
           )
 
