0199768b7671387b8f4b2d04b5e9bad920ea756a
gustavonsberg <github.thirsting835@simplelogin.fr>
2025-11-07 15:36:08 +0100

    Location accepts long for lat ang lng

--- app/src/main/java/ch/epfllife/model/map/Location.kt
+++ app/src/main/java/ch/epfllife/model/map/Location.kt
@@ -4,4 +4,20 @@ data class Location(
     val latitude: Double,
     val longitude: Double,
     val name: String,
-)
+) {
+  constructor(
+      latitude: Any,
+      longitude: Any,
+      name: String = ""
+  ) : this(parseDouble(latitude, "latitude"), parseDouble(longitude, "longitude"), name)
+
+  companion object {
+    private fun parseDouble(value: Any, fieldName: String): Double {
+      return when (value) {
+        is Double -> value
+        is Number -> value.toDouble()
+        else -> throw IllegalArgumentException("$fieldName has unsupported type: ${value::class}")
+      }
+    }
+  }
+}
