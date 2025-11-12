be9b2519f571522a93f4bf4889a6c7b203d66309
Mateo-Rico <mateo.rico.cdm@gmail.com>
2025-11-12 18:26:00 +0100

    home Screenn fix

--- app/src/main/java/ch/epfllife/ui/home/HomeScreen.kt
+++ app/src/main/java/ch/epfllife/ui/home/HomeScreen.kt
@@ -52,7 +52,7 @@ fun HomeScreen(modifier: Modifier = Modifier, viewModel: HomeViewModel = viewMod
                   name = "AeroPoly",
                   description = "Description",
                   eventCategory = EventCategory.ACADEMIC),
-          tags = setOf("workshop"),
+          tags = listOf("workshop"),
           price = Price(10u),
           pictureUrl =
               "https://www.shutterstock.com/image-photo/engineer-working-on-racing-fpv-600nw-2278353271.jpg")
