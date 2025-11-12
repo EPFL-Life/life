5ac17a545ec40b4836f08bde47dfd66b80d48b50
Mateo-Rico <mateo.rico.cdm@gmail.com>
2025-11-12 21:28:00 +0100

    EventDetailsViewModel fix

--- app/src/main/java/ch/epfllife/ui/eventDetails/EventDetailsViewModel.kt
+++ app/src/main/java/ch/epfllife/ui/eventDetails/EventDetailsViewModel.kt
@@ -12,6 +12,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import ch.epfllife.model.association.Association
 
 /** UI state for the EventDetails screen. */
 sealed class EventDetailsUIState {
