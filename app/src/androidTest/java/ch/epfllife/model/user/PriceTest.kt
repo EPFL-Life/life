a6d7cc8a5e08e98bfb6f34dafa2333e8f9ebb1e6
Mateo-Rico <mateo.rico.cdm@gmail.com>
2025-11-14 12:06:09 +0100

    feat: added test for Price

---
+++ app/src/androidTest/java/ch/epfllife/model/user/PriceTest.kt
@@ -0,0 +1,30 @@
+package ch.epfllife.model.user
+
+import org.junit.Assert
+import org.junit.Test
+
+class PriceTest {
+
+  fun assertFormatting(priceInCents: UInt, expected: String) {
+    val result = Price(priceInCents).formatPrice()
+    Assert.assertEquals(expected, result)
+  }
+
+  @Test
+  fun returnsFree() {
+    assertFormatting(priceInCents = 0u, expected = "Free")
+  }
+
+  @Test
+  fun noCents() {
+    assertFormatting(priceInCents = 300u, expected = "CHF 3.00")
+  }
+
+  @Test fun oneNumberCent() = assertFormatting(305u, "CHF 3.05")
+
+  @Test fun multipleFrancs() = assertFormatting(priceInCents = 1250u, expected = "CHF 12.50")
+
+  @Test fun smallPrices() = assertFormatting(priceInCents = 7u, expected = "CHF 0.07")
+
+  @Test fun completePrice() = assertFormatting(priceInCents = 123456u, expected = "CHF 1234.56")
+}
