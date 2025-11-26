f9c8cd83e689a2a6761655c2684d7a1f4a2b2171
Adriv020 <avicosan9@alumnes.ub.edu>
2025-11-26 11:34:29 +0100

    feat(ui): add LaunchedEffect to refresh composable state
    I needed to do this to refresh the list of enrolled events in the UI

--- app/src/main/java/ch/epfllife/ui/home/HomeScreen.kt
+++ app/src/main/java/ch/epfllife/ui/home/HomeScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -33,6 +34,8 @@ fun HomeScreen(
     viewModel: HomeViewModel = viewModel(),
     onEventClick: (eventId: String) -> Unit,
 ) {
+  LaunchedEffect(Unit) { viewModel.refresh() }
+
   var selected by remember { mutableStateOf(SubscriptionFilter.Subscribed) }
 
   val myEvents by viewModel.myEvents.collectAsState()
