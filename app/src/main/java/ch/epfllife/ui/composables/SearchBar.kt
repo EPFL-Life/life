bdf7abbd4bfca415538295e4a7880002ca5d58a2
daniel <git.daniel@protonmail.com>
2025-12-01 15:09:45 +0100

    fix: remove hardcoded string+color

--- app/src/main/java/ch/epfllife/ui/composables/SearchBar.kt
+++ app/src/main/java/ch/epfllife/ui/composables/SearchBar.kt
@@ -21,8 +21,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import ch.epfllife.R
 import ch.epfllife.ui.theme.Gray
 
 @Composable
@@ -51,9 +53,9 @@ fun SearchBar(
                   decorationBox = { innerTextField ->
                     if (query.isEmpty()) {
                       Text(
-                          text = "Search...",
+                          text = stringResource(R.string.search_bar_placeholder),
                           style = MaterialTheme.typography.bodyMedium,
-                          color = Color.Gray)
+                          color = MaterialTheme.colorScheme.onSurfaceVariant)
                     }
                     innerTextField()
                   })
