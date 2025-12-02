d12ef0c09f755bafc8b24abe1a0b9676eaa7a39a
kianbahia <kianhbahia@gmail.com>
2025-12-03 00:07:54 +0100

    format

--- app/src/main/java/ch/epfllife/model/map/NominatimLocationRepository.kt
+++ app/src/main/java/ch/epfllife/model/map/NominatimLocationRepository.kt
@@ -2,6 +2,7 @@ package ch.epfllife.model.map
 
 import android.util.Log
 import java.io.IOException
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.HttpUrl
@@ -12,7 +13,9 @@ import org.json.JSONArray
 class NominatimLocationRepository(
     private val client: OkHttpClient,
     private val userAgent: String = DEFAULT_USER_AGENT,
-    private val referer: String = DEFAULT_REFERER
+    private val referer: String =
+        DEFAULT_REFERER private
+            val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : LocationRepository {
 
   private fun parseBody(body: String): List<Location> {
@@ -28,7 +31,7 @@ class NominatimLocationRepository(
   }
 
   override suspend fun search(query: String): List<Location> =
-      withContext(Dispatchers.IO) {
+      withContext(dispatcher) {
         // Using HttpUrl.Builder to properly construct the URL with query parameters.
         val url =
             HttpUrl.Builder()
