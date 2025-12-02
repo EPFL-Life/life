38f3c304e8ba2df49658233654bbedbb0220fc60
MateoRicoCalvo <mateo.rico.cdm@gmail.com>
2025-12-02 23:54:09 +0100

    Create: It centrilized the configuration for all the web scrapers, were to scrape

---
+++ app/src/main/java/ch/epfllife/model/event_scraper/scrapers/website_config.py
@@ -0,0 +1,82 @@
+from models.event_models import Association, EventCategory
+
+ESN_EPFL_CONFIG = {
+    "name": "ESN EPFL Lausanne",
+    "url": "https://epfl.esn.ch/events",
+    "base_domain": "https://epfl.esn.ch",
+    "default_location": "EPFL Campus",
+    "coordinates": {"latitude": 46.5191, "longitude": 6.5668, "name": "EPFL Campus"},
+    
+    "selectors": {
+        
+        "event_container": [
+            ".views-row",                   
+            ".node.node-event",              
+            ".view-content .views-row"       
+        ],
+        
+       
+        "title": [
+            ".field-name-title h2",        
+            ".field-name-title h2 a",        
+            "h2",                            
+            ".title"                        
+        ],
+        
+     
+        "date": [
+            ".date-display-single",         
+            ".field-name-field-date",        
+            "[class*='date-display']",       
+            ".field-name-field-date span"    
+        ],
+        
+       
+        "location": [
+            ".field-name-field-location",    
+            "[class*='location']",           
+            ".location",                     
+            ".venue"                         
+        ],
+        
+       
+        "description": [
+            ".field-name-body",             
+            ".description",                  
+            "p",                             
+        ],
+        
+        
+        "price": [
+            ".field-name-field-price",       
+            "[class*='price']",              
+            ".price",                        
+            ".cost"                          
+        ],
+        
+       
+        "image": [
+            ".field-name-field-image img",   
+            ".group-image img",              
+            "img[src*='events']",           
+            "img"                           
+        ]
+    },
+    
+    "association": Association(
+        id="esn_epfl_lausanne",
+        name="ESN EPFL Lausanne",
+        description="Erasmus Student Network at EPFL Lausanne. We organize events for international students.",
+        event_category=EventCategory.SOCIAL,
+        picture_url="https://epfl.esn.ch/sites/epfl.esn.ch/files/logo_epfl.png",
+        logo_url="https://epfl.esn.ch/sites/epfl.esn.ch/files/logo_epfl.png",
+        about="ESN EPFL Lausanne helps international students integrate at EPFL through social and cultural events.",
+        social_links={
+            "website": "https://epfl.esn.ch",
+            "facebook": "https://facebook.com/esn.epfl",
+            "instagram": "https://instagram.com/esn_epfl"
+        }
+    )
+}
+
+ALL_WEBSITES = [ESN_EPFL_CONFIG]
