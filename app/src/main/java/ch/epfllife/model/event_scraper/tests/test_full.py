32bb55e48856665dedade089b79d0ebc478a251a
MateoRicoCalvo <mateo.rico.cdm@gmail.com>
2025-12-07 18:22:53 +0100

    Change: small changes so that the tests can run properly

--- app/src/main/java/ch/epfllife/model/event_scraper/tests/test_full.py
+++ app/src/main/java/ch/epfllife/model/event_scraper/tests/test_full.py
@@ -5,7 +5,7 @@ import sys
 import os
 import logging
 
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Configure logging
 logging.basicConfig(
