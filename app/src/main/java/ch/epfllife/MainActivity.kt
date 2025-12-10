75699fdd41e64c2d3c4d9e33581cd4ac384ebf5b
gustavonsberg <github.thirsting835@simplelogin.fr>
2025-12-10 23:42:48 +0100

    removed null result condition

--- app/src/main/java/ch/epfllife/MainActivity.kt
+++ app/src/main/java/ch/epfllife/MainActivity.kt
@@ -267,10 +267,10 @@ fun App(
                         handle[selectedAssociationNameKey] = updatedAssociation.name
                       }
                       navController.previousBackStackEntry?.savedStateHandle?.let(applySelection)
-                      val settingsEntry =
-                          runCatching { navController.getBackStackEntry(Screen.Settings.route) }
-                              .getOrNull()
-                      settingsEntry?.savedStateHandle?.let(applySelection)
+                      val settingsEntry = run {
+                        navController.getBackStackEntry(Screen.Settings.route)
+                      }
+                      settingsEntry.savedStateHandle.let(applySelection)
 
                       navController.popBackStack(Screen.Settings.route, false)
                     })
