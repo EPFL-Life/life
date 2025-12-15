052fa46215411df598bd84d0e5b12fc1b1d96a8f
kianbahia <kianhbahia@gmail.com>
2025-12-15 22:41:06 +0100

    Refactor event related tests

---
+++ app/src/androidTest/java/ch/epfllife/ui/eventDetails/AttendeeListScreenTest.kt
@@ -0,0 +1,25 @@
+package ch.epfllife.ui.eventDetails
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import ch.epfllife.example_data.ExampleUsers
+import ch.epfllife.ui.theme.Theme
+import org.junit.Rule
+import org.junit.Test
+
+class AttendeeListScreenTest {
+
+  @get:Rule val composeTestRule = createComposeRule()
+
+  @Test
+  fun showsHeaderAndAttendeeNames() {
+    val attendees = listOf(ExampleUsers.user1, ExampleUsers.user2)
+
+    composeTestRule.setContent { Theme { AttendeeListScreen(attendees = attendees, onBack = {}) } }
+
+    composeTestRule.onNodeWithText("Event Attendees").assertIsDisplayed()
+    composeTestRule.onNodeWithText(attendees[0].name).assertIsDisplayed()
+    composeTestRule.onNodeWithText(attendees[1].name).assertIsDisplayed()
+  }
+}
