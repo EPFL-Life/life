f71edfa506954c8a55d797046f3c4af15749d15c
kianbahia <kianhbahia@gmail.com>
2025-12-17 00:33:24 +0100

    Address PR nitpick

--- app/src/main/java/ch/epfllife/ui/composables/EventCard.kt
+++ app/src/main/java/ch/epfllife/ui/composables/EventCard.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -47,10 +48,7 @@ fun EventCard(
     modifier: Modifier = Modifier,
     onClick: () -> Unit,
 ) {
-  val shortLocation =
-      androidx.compose.runtime.remember(event.location.name) {
-        formatLocationForCard(event.location.name)
-      }
+  val shortLocation = remember(event.location.name) { formatLocationForCard(event.location.name) }
 
   Card(
       onClick = onClick,
