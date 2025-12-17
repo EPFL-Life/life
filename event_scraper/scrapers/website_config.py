2aee56402595e9582f398462e0281848652cd272
MateoRicoCalvo <mateo.rico.cdm@gmail.com>
2025-12-17 02:26:37 +0100

    add: information on scraping AgePoly website

--- event_scraper/scrapers/website_config.py
+++ event_scraper/scrapers/website_config.py
@@ -33,6 +33,25 @@ class SelectorConfig:
         
         value = getattr(self, key, [])
         return value if value else []
+    
+    def to_dict(self) -> dict:
+        """Convert to dictionary for WebScraper compatibility"""
+        return {
+            "event_container": self.event_container,
+            "title": self.title,
+            "date": self.date,
+            "location": self.location,
+            "description": self.description,
+            "price": self.price,
+            "image": self.image,
+            "event_link": self.event_link,
+            "detailed_title": self.detailed_title,
+            "detailed_date": self.detailed_date,
+            "detailed_description": self.detailed_description,
+            "detailed_location": self.detailed_location,
+            "detailed_price": self.detailed_price,
+            "detailed_image": self.detailed_image,
+        }
 
 @dataclass
 class WebsiteConfig:
@@ -51,7 +70,7 @@ ESN_EPFL_CONFIG = WebsiteConfig(
     url="https://epfl.esn.ch/events",
     base_domain="https://epfl.esn.ch",
     default_location=DEFAULT_LOCATION_NAME,
-    coordinates={"latitude": DEFAULT_COORDINATES.copy()},
+    coordinates=DEFAULT_COORDINATES.copy(),
     
     selectors=SelectorConfig(
         event_container=[".views-row", ".node.node-event", ".view-content .views-row"],
@@ -86,4 +105,64 @@ ESN_EPFL_CONFIG = WebsiteConfig(
     )
 )
 
-ALL_WEBSITES = [ESN_EPFL_CONFIG]
+AGEPOLY_CONFIG = WebsiteConfig(
+    name="AGEPoly EPFL",
+    url="https://agepoly.ch/en/evenements/",
+    base_domain="https://agepoly.ch",
+    default_location=DEFAULT_LOCATION_NAME,
+    coordinates=DEFAULT_COORDINATES.copy(),
+    
+    selectors=SelectorConfig(
+        # Event container - the main wrapper for each event
+        event_container=[".o-posts-grid-post-body"],
+        
+        # Title is in h4 with a link inside
+        title=["h4.o-posts-grid-post-title", "h4.o-posts-grid-post-title a"],
+        
+        # Date is in a <time> element within the meta paragraph
+        date=[".o-posts-grid-post-meta time", "time"],
+        
+        # Location - not shown in list view, use default EPFL location
+        location=[".field-name-field-location", "[class*='location']", ".location", ".venue"],  # Will use default location
+        
+        # Description is in this specific div
+        description=[".o-posts-grid-post-description"],
+        
+        # Price - not shown in list view
+        price=[".field-name-field-price", "[class*='price']", ".price", ".cost"],  # Default to free
+        
+        # Image - no images in list view
+        image=[".field-name-field-image img", ".group-image img", "img[src*='events']", "img"],
+        
+        # Link to detailed page is in the title anchor tag
+        event_link=["h4.o-posts-grid-post-title a"],
+        
+        # Detailed page selectors - we'll need to inspect individual event pages
+        # For now, using generic WordPress selectors
+        detailed_title=["h1.entry-title", "h1"],
+        detailed_date=[".entry-meta time", "time.entry-date", "time"],
+        detailed_description=[".entry-content", ".post-content", "article"],
+        detailed_location=[".event-location", ".venue", ".location"],
+        detailed_price=[".price", ".cost", ".event-price"],
+        detailed_image=[".wp-post-image", "img.attachment-post-thumbnail", ".entry-content img"],
+    ),
+    
+    association=Association(
+        id="agepoly_epfl",
+        name="AGEPoly EPFL",
+        description="The student association of the School of Engineering at EPFL. Organizes academic, career, and social events.",
+        event_category=EventCategory.SOCIAL,  # Most Agepoly events are social
+        # Found these URLs from inspecting the Agepoly site
+        picture_url="https://agepoly.ch/wp-content/uploads/2023/09/banniere-agepoly.jpg",
+        logo_url="https://agepoly.ch/wp-content/themes/agepoly/assets/images/logo.svg",
+        about="AGEPoly represents engineering students at EPFL and organizes a wide range of events including foodtruck challenges, ski sales, and social gatherings.",
+        social_links={
+            "website": "https://agepoly.ch",
+            "facebook": "https://www.facebook.com/AGEPoly",
+            "instagram": "https://www.instagram.com/agepoly_epfl/",
+            "linkedin": "https://www.linkedin.com/company/agepoly/"
+        }
+    )
+)
+
+ALL_WEBSITES = [ESN_EPFL_CONFIG , AGEPOLY_CONFIG]
