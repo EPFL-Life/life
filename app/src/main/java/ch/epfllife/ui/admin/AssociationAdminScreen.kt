93f6efdc753a25dd2807ca09bf9e49b6744e9b80
daniel <git.daniel@protonmail.com>
2025-12-17 05:41:32 +0100

    ui: added icons to improve ui

--- app/src/main/java/ch/epfllife/ui/admin/AssociationAdminScreen.kt
+++ app/src/main/java/ch/epfllife/ui/admin/AssociationAdminScreen.kt
@@ -3,15 +3,22 @@ package ch.epfllife.ui.admin
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.FormatListNumbered
+import androidx.compose.material.icons.filled.House
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -32,10 +39,10 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import ch.epfllife.R
 import ch.epfllife.model.authentication.Auth
 import ch.epfllife.model.db.Db
+import ch.epfllife.ui.composables.BackButton
 import ch.epfllife.ui.composables.SettingsButton
 import ch.epfllife.ui.settings.SettingsViewModel
 import ch.epfllife.ui.theme.DangerRed
-import ch.epfllife.ui.theme.LifeRed
 import ch.epfllife.utils.SystemToastHelper
 import ch.epfllife.utils.ToastHelper
 
@@ -98,77 +105,84 @@ fun AssociationAdminScreen(
         })
   }
 
-  Column(
-      modifier =
-          modifier
-              .fillMaxSize()
-              .padding(horizontal = 15.dp)
-              .testTag(AssociationAdminScreenTestTags.SCREEN),
-      horizontalAlignment = Alignment.CenterHorizontally,
-      verticalArrangement = Arrangement.Top) {
-        Spacer(Modifier.height(24.dp))
+  Box(modifier = modifier.fillMaxSize()) {
+    Column(
+        modifier =
+            Modifier.fillMaxSize()
+                .padding(horizontal = 15.dp)
+                .testTag(AssociationAdminScreenTestTags.SCREEN),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top) {
+          Spacer(Modifier.height(72.dp)) // Padding for BackButton
 
-        Text(
-            text = stringResource(R.string.admin_console),
-            style = MaterialTheme.typography.headlineMedium)
+          Text(
+              text = stringResource(R.string.admin_console),
+              style = MaterialTheme.typography.headlineMedium)
 
-        Spacer(Modifier.height(32.dp))
+          Spacer(Modifier.height(32.dp))
 
-        SettingsButton(
-            text =
-                associationName?.let { stringResource(R.string.settings_selected_association, it) }
-                    ?: stringResource(R.string.settings_screen_association),
-            onClick = onSelectAssociationClick,
-            modifier =
-                Modifier.fillMaxWidth()
-                    .testTag(AssociationAdminScreenTestTags.SELECT_ASSOCIATION_BUTTON))
-
-        Spacer(Modifier.height(16.dp))
-
-        if (associationId != null) {
           SettingsButton(
-              text = stringResource(R.string.manage_association, associationName ?: ""),
-              onClick = { onManageAssociationClick(associationId) },
+              text =
+                  associationName?.let {
+                    stringResource(R.string.settings_selected_association, it)
+                  } ?: stringResource(R.string.settings_screen_association),
+              icon = Icons.Filled.House,
+              onClick = onSelectAssociationClick,
               modifier =
                   Modifier.fillMaxWidth()
-                      .testTag(AssociationAdminScreenTestTags.MANAGE_ASSOCIATION_BUTTON))
+                      .testTag(AssociationAdminScreenTestTags.SELECT_ASSOCIATION_BUTTON))
 
           Spacer(Modifier.height(16.dp))
 
-          SettingsButton(
-              text = stringResource(R.string.manage_events_title),
-              onClick = { onManageAssociationEventsClick(associationId) },
-              modifier =
-                  Modifier.fillMaxWidth()
-                      .testTag(AssociationAdminScreenTestTags.MANAGE_EVENTS_BUTTON))
+          if (associationId != null) {
+            SettingsButton(
+                text = stringResource(R.string.manage_association, associationName ?: ""),
+                icon = Icons.Default.Edit,
+                onClick = { onManageAssociationClick(associationId) },
+                modifier =
+                    Modifier.fillMaxWidth()
+                        .testTag(AssociationAdminScreenTestTags.MANAGE_ASSOCIATION_BUTTON))
 
-          Spacer(Modifier.height(16.dp))
+            Spacer(Modifier.height(16.dp))
 
-          Button(
-              modifier =
-                  Modifier.fillMaxWidth()
-                      .testTag(AssociationAdminScreenTestTags.DELETE_ASSOCIATION_BUTTON),
-              onClick = { showDeleteDialog = true },
-              shape = RoundedCornerShape(6.dp),
-              colors =
-                  ButtonDefaults.buttonColors(
-                      containerColor = LifeRed, contentColor = Color.White)) {
-                Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.CenterStart) {
-                  Text(
-                      text = stringResource(R.string.delete_association_button),
-                      style =
-                          MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.SemiBold))
+            SettingsButton(
+                text = stringResource(R.string.manage_events_title),
+                icon = Icons.Default.FormatListNumbered,
+                onClick = { onManageAssociationEventsClick(associationId) },
+                modifier =
+                    Modifier.fillMaxWidth()
+                        .testTag(AssociationAdminScreenTestTags.MANAGE_EVENTS_BUTTON))
+
+            Spacer(Modifier.height(16.dp))
+
+            Button(
+                modifier =
+                    Modifier.fillMaxWidth()
+                        .testTag(AssociationAdminScreenTestTags.DELETE_ASSOCIATION_BUTTON),
+                onClick = { showDeleteDialog = true },
+                shape = RoundedCornerShape(12.dp),
+                elevation = ButtonDefaults.elevatedButtonElevation(defaultElevation = 5.dp),
+                colors =
+                    ButtonDefaults.buttonColors(
+                        containerColor = DangerRed, contentColor = Color.White)) {
+                  Row(
+                      horizontalArrangement = Arrangement.Start,
+                      verticalAlignment = Alignment.CenterVertically,
+                      modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)) {
+                        Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = null,
+                            modifier = Modifier.padding(end = 16.dp))
+                        Text(
+                            text = stringResource(R.string.delete_association_button),
+                            style =
+                                MaterialTheme.typography.bodyLarge.copy(
+                                    fontWeight = FontWeight.SemiBold))
+                      }
                 }
-              }
+          }
         }
 
-        Spacer(Modifier.height(16.dp))
-
-        Button(
-            modifier = Modifier.fillMaxWidth().testTag(AssociationAdminScreenTestTags.BACK_BUTTON),
-            onClick = onGoBack,
-            shape = RoundedCornerShape(6.dp)) {
-              Text(stringResource(R.string.back_button_description))
-            }
-      }
+    BackButton(modifier = Modifier.align(Alignment.TopStart), onGoBack = onGoBack)
+  }
 }
